e16305c08a5b7ca99c7a73f1236e2a9e23b2776a
Onur Sinan Togrul <o.sinantogrul@azerion.com>
2022-09-10 02:12:27 +0300

    Finished Random card Generation

--- Assets/_deck-player/Scripts/Managers/CardManager.cs
+++ Assets/_deck-player/Scripts/Managers/CardManager.cs
@@ -5,21 +5,51 @@ using DeckPlayer.CardSystem;
 
 public class CardManager : MonoBehaviour
 {
-    public List<Card> cards; // 11 cards
-    private CardDataCollection collection; // 52 card datas
+    [Header("Card-Manager Config")]
+    public int initialCardAmount;
+    public GameObject cardPrefab;
+    public Transform cardDeck;
+    
+    public List<CardData> currentCardDatas; // 11 cards
+    private CardDataCollection cardCollection; // 52 card datas
+    private List<Transform> cardSlots;
 
     private void Awake()
     {
-        collection = Resources.Load<CardDataCollection>("CardData/CardDataCollection");
+        cardCollection = Resources.Load<CardDataCollection>("CardData/CardDataCollection");
+        cardSlots = new List<Transform>();  
+        PrepareDeck();
     }
 
     private void Start()
     {
-        
+        SelectCards();
+    }
+    public void PrepareDeck()
+    {
+        foreach(Transform cardSlot in cardDeck)
+            cardSlots.Add(cardSlot); // TODO: Make this dynamic
     }
 
-    private void CreateCard(CardData cardData)
+    private void SelectCards()
     {
-        
+        CardData[] collection = cardCollection.GetCollection();
+        int collectionAmount = collection.Length;
+
+        for (int i = 0; i < initialCardAmount; i++)
+        {
+            int randomIndex = Random.Range(0, collectionAmount);
+            currentCardDatas.Add(collection[randomIndex]);
+            GameObject newCard = CreateCard(collection[randomIndex]);
+            newCard.transform.SetParent(cardSlots[i], false);
+        }
+    }
+
+    private GameObject CreateCard(CardData cardData)
+    {
+        GameObject cardObject = Instantiate(cardPrefab);
+        Card card = cardObject.GetComponent<Card>();
+        card.InitCard(cardData);
+        return cardObject;
     }
 }
